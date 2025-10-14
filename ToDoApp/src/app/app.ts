f7195307c81b6f61e1ce959406654a143cd78f9f
ErnAbr <ern_abr@yahoo.com>
2025-10-14 19:58:47 +0300

    app init and tailwind setup

---
+++ ToDoApp/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './templates/main.html',
+  imports: [],
+})
+export class App {
+  name = 'Ernestas';
+}
