c609caeb0dcd5016165e8cd274366777664d022e
ErnAbr <ern_abr@yahoo.com>
2025-10-24 20:45:09 +0300

    line-through todo tasks and undo the line-through

--- ToDoApp/src/app/components/ListView/ListView.ts
+++ ToDoApp/src/app/components/ListView/ListView.ts
@@ -1,10 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { Button } from '../Button/Button';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'list-view',
   templateUrl: './listView.html',
-  imports: [],
+  imports: [Button, NgClass],
 })
 export class ListView {
   @Input() toDoList: string[] = [];
+  isCompleted: boolean[] = [];
+
+  deleteItem(index: number) {
+    this.toDoList.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    if (this.isCompleted[index] == true) {
+      this.isCompleted[index] = false;
+    } else {
+      this.isCompleted[index] = true;
+    }
+  }
 }
+
+//line-through
